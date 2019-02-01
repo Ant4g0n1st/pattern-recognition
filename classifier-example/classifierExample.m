6d976f3c3111b27354f034bab4818432b8024a91
Ant4g0n1st <dragon124victor421force@gmail.com>
2019-02-01 16:35:25 -0600

    Plotting points.

--- classifier-example/classifierExample.m
+++ classifier-example/classifierExample.m
@@ -1,7 +1,9 @@
+clear
+close
 clc
 
-radius = 10 ^ 9;
-classes = 6;
+radius = 10 ^ 2;
+classes = 12;
 
 step = (2 * pi) / classes;
 degrees = step : step : 2 * pi;
@@ -12,7 +14,7 @@ for k = 1 : classes
     centers(:, k) = [ cos(degrees(k)) ; sin(degrees(k)) ] .* radius;
 end
 
-classRadius = 10 ^ 3;
+classRadius = 10 ^ 1;
 classElements = 4;
 
 step = (2 * pi) / classElements;
@@ -23,19 +25,24 @@ classifiers = zeros(2, classElements, classes);
 for k = 1 : classes
     for l = 1 : classElements
         element = [ cos(degrees(l)) ; sin(degrees(l))] .* classRadius;
-        element = centers(k) + element;
+        element = centers(:, k) + element;
         classifiers(:, l, k) = element;
     end
 end
 
 means = zeros(2, classes);
+grid on
+hold on
 
 for k = 1 : classes
+    plot(classifiers(1, :, k), classifiers(2, :, k), 'ro');
     means(:, k) = mean(classifiers(:, :, k), 2);
 end
 
-scale = 10 ^ 9;
-tests = 5;
+plot(means(1, :), means(2, :), 'go');
+
+scale = 5 * 10 ^ 1;
+tests = 100;
 
 for k = 1 : tests
     testPoint = [ randn() ; randn() ] .* scale;
@@ -48,7 +55,8 @@ for k = 1 : tests
             minClass = l;
         end
     end
-    disp(['Point ', mat2str(testPoint) ,' belongs to class ', num2str(minClass)]);
+    disp(['El vector ', mat2str(testPoint) ,' pertenece a la clase ', num2str(minClass)]);
+    plot(testPoint(1, :), testPoint(2, :), 'bo');
 end
 
 clear
