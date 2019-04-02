a6a2c31f7d686c71cdff8a62d96f6f8ea0864462
Ant4g0n1st <dragon124victor421force@gmail.com>
2019-04-02 17:39:27 -0600

    K-Means on images.

--- k-means/kMeansClustering.m
+++ k-means/kMeansClustering.m
@@ -2,26 +2,62 @@ close
 clear
 clc
 
+%legend
+
+figure(1);
+img = imread("kMeansInput.png"); % Germany Flag
+%img = imread("kMeansInput.jpg"); % Italy Flag
+%img = imread("kMeansInput1.jpg"); % France Flag
+%img = rgb2gray(img);
+imshow(img);
+hold on
+
+[ dx, dy, dz ] = size(img);
+
 disp("Algoritmo K-Means.");
 
-n = input("Ingrese el número de vectores : ");
-m = input("Ingrese el valor de K : ");
+axes = 2;
+n = 5000;
+m = 3;
 
 threshold = 1e-9;
-dim = 2;
+dim = 3;
 
-vectors = randn(n, dim);
+pixels = rand(n, axes);
+pixels(:, 1) = floor(pixels(:, 1) * (dx - 1) + 1);
+pixels(:, 2) = floor(pixels(:, 2) * (dy - 1) + 1);
+pixels(:, :) = pixels(randperm(n), :);
 
-means = vectors(1 : m, :);
+vectors = zeros(n, dim);
+for k = 1 : n
+    vectors(k, 1) = img(pixels(k, 1), pixels(k, 2), 1);
+    vectors(k, 2) = img(pixels(k, 1), pixels(k, 2), 2);
+    vectors(k, 3) = img(pixels(k, 1), pixels(k, 2), 3);
+end
+
+
+means = zeros(m, dim);
+for k = 1 : m
+    for x = 1 : n
+        inMeans = 0;
+        for y = 1 : k - 1
+            if means(y, :) == vectors(x, :)
+                inMeans = 1;
+                continue
+            end
+        end
+        if inMeans == 1
+            continue
+        end
+        means(k, :) = vectors(x, :);
+        break
+    end
+end
 
 prev = zeros(m, dim);
 prev(:, :) = Inf;
 
 it = 0;
-
-hold on
-grid on
-legend
 
 while 1
     it = it + 1;
@@ -62,14 +98,16 @@ while 1
     %end
     %fprintf("\n");
     if change == 0
-        for k = 1 : m
-            cluster = vectors(clusters{k}, :);
-            scatter(cluster(:, 1), cluster(:, 2), 'filled', 'DisplayName', strcat("C", num2str(k)));
-        end
-        scatter(means(:, 1), means(:, 2), 'ko', 'DisplayName', 'Medias');
         break
     end
     prev(:, :) = means(:, :);
+end
+
+for k = 1 : m
+    cluster = pixels(clusters{k}, :);
+    [s, ~] = size(cluster);
+    scatter(cluster(:, 2), cluster(:, 1), 'filled');
+    fprintf("Cluster %d size: %d\n", k, s);
 end
 
 fprintf("Se logró en %d iteraciones.\n", it);
